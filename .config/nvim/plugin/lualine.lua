ed6d645586ab9b29b6ebff4d339a271f1f28c8af
Russell Waite <russellwaite@gmail.com>
2023-03-03 10:49:54 +0000

    switch out C-t for new tab, A-t for open in trouble

--- .config/nvim/plugin/lualine.lua
+++ .config/nvim/plugin/lualine.lua
@@ -3,65 +3,67 @@
 -- local non_language_ft = {'fugitive', 'startify'}
 
 require('lualine').setup({
-  options = {
-    theme = "tokyonight",
-    -- Separators might look weird for certain fonts (eg Cascadia)
-    -- component_separators = {left = '', right = ''},
-    component_separators = '|',
-    section_separators = {left = '', right = ''},
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {
-        {
-            'diagnostics',
-            sources = {'nvim_diagnostic'},
-            sections = {'error', 'warn', 'info'},
+    options = {
+        theme = "tokyonight",
+        -- Separators might look weird for certain fonts (eg Cascadia)
+        -- component_separators = {left = '', right = ''},
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+            -- {
+            --     'diagnostics',
+            --     sources = { 'nvim_diagnostic' },
+            --     sections = { 'error', 'warn', 'info' },
+            -- },
+            {
+                function() return vim.fn.bufnr('%') end
+            }
         },
+        lualine_c = {
+            'filetype',
+            { 'branch' },
+            {
+                function()
+                    local msg = 'No LSP'
+                    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                    local clients = vim.lsp.get_active_clients()
+
+                    if next(clients) == nil then
+                        return msg
+                    end
+
+                    -- Check for utility buffers
+                    for ft in non_language_ft do
+                        if ft:match(buf_ft) then
+                            return ''
+                        end
+                    end
+
+                    for _, client in ipairs(clients) do
+                        local filetypes = client.config.filetypes
+
+                        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                            -- return 'LSP:'..client.name  -- Return LSP name
+                            return '' -- Only display if no LSP is found
+                        end
+                    end
+
+                    return msg
+                end,
+                color = { fg = '#ffffff', gui = 'bold' },
+                separator = "",
+            },
+            { function() return require 'lsp-status'.status() end },
+        },
+        lualine_x = { 'filename', 'encoding' },
+        lualine_y = { 'progress' },
+        -- lualine_z = {
+        --   {function () return '' end},
+        --   {'location'},
+        -- }
+        lualine_z = { 'location' },
     },
-    lualine_c = {
-      'filetype',
-      {'branch'},
-      {
-        function()
-          local msg = 'No LSP'
-          local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-          local clients = vim.lsp.get_active_clients()
-
-          if next(clients) == nil  then
-            return msg
-          end
-
-          -- Check for utility buffers
-          for ft in non_language_ft do
-            if ft:match(buf_ft) then
-              return ''
-            end
-          end
-
-          for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-              -- return 'LSP:'..client.name  -- Return LSP name
-              return ''  -- Only display if no LSP is found
-            end
-          end
-
-          return msg
-        end,
-        color = {fg = '#ffffff', gui = 'bold'},
-        separator = "",
-      },
-      {function() return require'lsp-status'.status() end},
-    },
-    lualine_x = {'filename', 'encoding'},
-    lualine_y = {'progress'},
-    -- lualine_z = {
-    --   {function () return '' end},
-    --   {'location'},
-    -- }
-    lualine_z = { 'location' },
-  },
 })
-
--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -4,49 +4,51 @@ local trouble = require("trouble.providers.telescope")
 local telescope = require("telescope")
 
 telescope.setup {
-  defaults = {
-    mappings = {
-      i = {
-				["<c-t>"] = trouble.open_with_trouble,
-				["<c-h>"] = "which_key"
-      },
-      n = { ["<c-t>"] = trouble.open_with_trouble },
-    },
-  },
-  extensions = {
-    ["file_browser"] = {
-      theme = "ivy",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-          -- your custom insert mode mappings
+    defaults = {
+        mappings = {
+            i = {
+                ["<A-t>"] = trouble.open_with_trouble,
+                ["<c-h>"] = "which_key",
+            },
+            n = {
+                ["<A-t>"] = trouble.open_with_trouble,
+            },
         },
-        ["n"] = {
-          -- your custom normal mode mappings
-        },
-      },
     },
-    ["ui-select"] = {
-      require("telescope.themes").get_dropdown {
-        -- even more opts
-      }
+    extensions = {
+        ["file_browser"] = {
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+                ["i"] = {
+                    -- your custom insert mode mappings
+                },
+                ["n"] = {
+                    -- your custom normal mode mappings
+                },
+            },
+        },
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+                -- even more opts
+            }
 
-      -- pseudo code / specification for writing custom displays, like the one
-      -- for "codeactions"
-      -- specific_opts = {
-      --   [kind] = {
-      --     make_indexed = function(items) -> indexed_items, width,
-      --     make_displayer = function(widths) -> displayer
-      --     make_display = function(displayer) -> function(e)
-      --     make_ordinal = function(e) -> string
-      --   },
-      --   -- for example to disable the custom builtin "codeactions" display
-      --      do the following
-      --   codeactions = false,
-      -- }
+            -- pseudo code / specification for writing custom displays, like the one
+            -- for "codeactions"
+            -- specific_opts = {
+            --   [kind] = {
+            --     make_indexed = function(items) -> indexed_items, width,
+            --     make_displayer = function(widths) -> displayer
+            --     make_display = function(displayer) -> function(e)
+            --     make_ordinal = function(e) -> string
+            --   },
+            --   -- for example to disable the custom builtin "codeactions" display
+            --      do the following
+            --   codeactions = false,
+            -- }
+        }
     }
-  }
 }
 
 require('telescope').load_extension('fzf') -- load native
